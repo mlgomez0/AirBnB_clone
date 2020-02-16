a6535e9450dafed25a5473592aee7ce6f4cba90f
mlgomez0 <mlgomez230@hotmail.com>
2020-02-16 17:12:33 +0000

    7- adding all, show, update

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 """ This module contains out line command tnterpreter for the ABNB projecrt
 """
 
-
+from ast import literal_eval
 import models
 from models import storage
 from cmd import Cmd
@@ -15,7 +15,7 @@ class HBNBCommand(Cmd):
     prompt = '(hbnb) '
 
     def do_create(self, inp):
-        """ Creates a new instance of BaseModel, saves it (to the JSON file) and 
+        """ Creates a new instance of BaseModel, saves it (to the JSON file) and
             prints the id. Ex: $ create BaseModel
         """
         if inp == "":
@@ -26,9 +26,9 @@ class HBNBCommand(Cmd):
             new_obj = BaseModel()
             new_obj.save()
             print(new_obj.id)
-        
+
     def do_show(self, inp):
-        """ Prints the string representation of an instance based on the class 
+        """ Prints the string representation of an instance based on the class
             name and id.
         """
         flag = 0
@@ -48,18 +48,86 @@ class HBNBCommand(Cmd):
                     flag = 1
             if flag == 0:
                 print("** no instance found **")
-                
-            
-            
+
+    def do_destroy(self, inp):
+        """Deletes an instance based on the class name and id (save the
+           change into the JSON file).
+        """
+        flag = 0
+        list_args = inp.split(' ')
+        if list_args[0] == "":
+            print("** class name missing **")
+        elif list_args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list_args) <= 1:
+            print("** instance id missing **")
+        else:
+            all_objs = storage.all()
+            for k, v in all_objs.items():
+                splitted = k.split('.')[1]
+                if splitted == list_args[1]:
+                    del all_objs[k]
+                    flag = 1
+                    storage.save()
+                    break
+            if flag == 0:
+                print("** no instance found **")
+
+    def do_all(self, inp):
+        """Prints all string representation of all
+           instances based or not on the class name.
+        """
+        my_list=[]
+        if inp != "" and inp != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            all_objs = storage.all()
+            for k, v in all_objs.items():
+                my_list.append(v.__str__())
+            print(my_list)
+
+    def do_update(self, inp):
+        """Updates an instance based on the class name and id by
+           adding or updating attribute
+        """
+       	flag = 0
+        list_args = inp.split(' ')
+        if list_args[0] == "":
+            print("** class name missing **")
+        elif list_args[0] != "BaseModel":
+            print("** class doesn't exist **")
+        elif len(list_args) == 1:
+            print("** instance id missing **")
+        elif len(list_args) == 2:
+            print("** attribute name missing **")
+        elif len(list_args) <= 3:
+            print("** value missing **")
+        else:
+            all_objs = storage.all()
+            for k, v in all_objs.items():
+                splitted = k.split('.')[1]
+                if splitted == list_args[1]:
+                    flag = 1
+                    if (type(literal_eval(list_args[3]))) == int:
+                        setattr(v, list_args[2], int(list_args[3]))
+                    elif (type(literal_eval(list_args[3]))) == float:
+                        setattr(v, list_args[2], float(list_args[3]))
+                    else:
+                        setattr(v, list_args[2], list_args[3][1:-1])
+                    storage.save()
+                    break
+            if flag == 0:
+                print("** no instance found **")
+
     def do_quit(self, inp):
         """
         Quit command to exit the program
         """
         return True
-        
+
     def help_quit(quit):
         print('Quit command to exit the program')
-        
+
     def emptyline(self):
          pass
 
